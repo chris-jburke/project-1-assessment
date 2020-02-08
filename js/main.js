e67f9d116880eb76a06cc11ffbffce6a7169c2e5
chris-jburke <cjburke04@gmail.com>
2020-02-07 16:19:59 -0800

    fixed issues

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
 let count = 0;
 
 document.addEventListener("DOMContentLoaded", function(){
-	console.log("Here");
 	document.querySelector(".increment").addEventListener("click", function(){
 		let domAmount = document.getElementsByTagName("input")[0].value;
-		count += parseInt(domAmount);
-		console.log(count);
-		let domCount = document.querySelector(".count");
-		if(count < 0){
-			domCount.classList.add("negative");
-		} else {
-			domCount.classList.remove("negative");
+		let amount = parseInt(domAmount);
+		let test = count + amount;
+		if(test) {
+			count += parseInt(domAmount);
+			let domCount = document.querySelector(".count");
+			if(count < 0){
+				domCount.classList.add("negative");
+			} else {
+				domCount.classList.remove("negative");
+			}
+			domCount.innerText = count;
+
 		}
-		domCount.innerText = count;
 
 	})
 	document.querySelector(".decrement").addEventListener("click", function(){
 		let domAmount = document.getElementsByTagName("input")[0].value;
-		count -= parseInt(domAmount);
-		let domCount = document.querySelector(".count");
-		if(count < 0){
-			domCount.classList.add("negative");
-		} else {
-			domCount.classList.remove("negative");
+		let amount = parseInt(domAmount);
+		let test = count + amount;
+		if(test){
+		
+			count -= parseInt(domAmount);
+			let domCount = document.querySelector(".count");
+			if(count < 0){
+				domCount.classList.add("negative");
+			} else {
+				domCount.classList.remove("negative");
+			}
+			domCount.innerText = count;
 		}
-		domCount.innerText = count;
 	})
 })
